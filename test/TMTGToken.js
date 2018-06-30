efb6d2c256cfd434dc2841998de426b15c515cfb
HANE <inspiration33@naver.com>
2018-06-30 20:11:08 +0900

    이름 변경

--- test/TMTGToken.js
+++ test/TMTGToken.js
@@ -4,8 +4,8 @@
  * 1. Init engine and its components
  */
 // 
-const Engine = require('../mods/hane.engine')
-Engine.core.init(require('../mods/hane.config'))
+const Engine = require('../mods/dgex.engine')
+Engine.core.init(require('../mods/dgex.config'))
 Engine.core.components(
     require('winston'),
     require('fs')
@@ -13,16 +13,16 @@ Engine.core.components(
 Engine.core.build()
 
 /**
- * 2. Create Hane object
- * Hane = Han Sang Il
+ * 2. Create dgex object
+ * dgex = Han Sang Il
  */
-const Hane = require('../mods/hane.js')
-Hane.core.init(Engine)
-Hane.core.components(
+const dgex = require('../mods/dgex.js')
+dgex.core.init(Engine)
+dgex.core.components(
     artifacts.require('../contracts/TMTGToken.sol')
 )
 
 /**
  * 3. Choose any file from the sub folder
  */
-Hane.core.run();
+dgex.core.run();
